875412ad4c88d579a99dc0251cc3149010591077
hewr srood <hewr_s@outlook.com>
2021-02-16 14:15:00 +0300

    fix some bugs

--- src/components/AbouWatches/WatchCarousel.js
+++ src/components/AbouWatches/WatchCarousel.js
@@ -1,61 +1,70 @@
-import React, { useState, useEffect } from "react";
-import { Carousel } from "react-bootstrap";
-import { FaSearchPlus } from "react-icons/fa";
-import FsLightbox from "fslightbox-react";
-const Wathes = [
+import React, { useState, useEffect } from 'react';
+import { Carousel } from 'react-bootstrap';
+import { FaSearchPlus } from 'react-icons/fa';
+import FsLightbox from 'fslightbox-react';
+const watches = [
   {
     url:
-      "https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/gallery7.jpg",
-    alt: "First Watch",
+      'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/gallery7.jpg',
+    alt: 'First Watch',
   },
   {
     url:
-      "https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/gallery6.jpg",
-    alt: "First Watch",
+      'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/gallery6.jpg',
+    alt: 'First Watch',
   },
 
   {
     url:
-      "https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/gallery5.jpg",
-    alt: "First Watch",
+      'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/gallery5.jpg',
+    alt: 'First Watch',
   },
   {
     url:
-      "https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite5.jpg",
-    alt: "First Watch",
+      'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite5.jpg',
+    alt: 'First Watch',
   },
   {
     url:
-      "https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite4.jpg",
-    alt: "First Watch",
+      'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite4.jpg',
+    alt: 'First Watch',
   },
   {
     url:
-      "https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite3.jpg",
-    alt: "First Watch",
+      'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite3.jpg',
+    alt: 'First Watch',
   },
   {
     url:
-      "https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite3.jpg",
-    alt: "First Watch",
+      'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite3.jpg',
+    alt: 'First Watch',
   },
   {
     url:
-      "https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite2.jpg",
-    alt: "First Watch",
+      'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite2.jpg',
+    alt: 'First Watch',
   },
   {
     url:
-      "https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite.jpg",
-    alt: "First Watch",
+      'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite.jpg',
+    alt: 'First Watch',
   },
 ];
-let watchUrls = [];
+let watchUrls = [
+  'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/gallery7.jpg',
+  'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/gallery6.jpg',
+  'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/gallery5.jpg',
+  'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite5.jpg',
+  'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite4.jpg',
+  'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite3.jpg',
+  'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite3.jpg',
+  'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite2.jpg',
+  'https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/29/2018/03/Watchwhite.jpg',
+];
 
 export default function WatchCarousel() {
-  const [toggler, setToggler] = useState(false);
   const [numberOfWatches, setNumberOfWatches] = useState(3);
-  const [imageSize, setimageSize] = useState("30%");
+  const [imageSize, setimageSize] = useState('30%');
   const [lightboxController, setLightboxController] = useState({
     toggler: false,
     sourceIndex: 0,
@@ -69,59 +78,51 @@ export default function WatchCarousel() {
   useEffect(() => {
     if (window.innerWidth < 768) {
       setNumberOfWatches(2);
-      setimageSize("40%");
+      setimageSize('40%');
     }
   }, []);
-  window.addEventListener("resize", () => {
+  window.addEventListener('resize', () => {
     if (window.innerWidth < 768) {
       setNumberOfWatches(2);
-      setimageSize("42%");
+      setimageSize('42%');
     } else {
       setNumberOfWatches(3);
-      setimageSize("32%");
+      setimageSize('32%');
     }
   });
   let CItems = [];
-  Wathes.map((watch, i) => {
-    if (i % numberOfWatches !== 0) {
-      CItems[CItems.length - 1].push(watch);
-    } else {
-      CItems.push([watch]);
-    }
+  watches.map((watch, i) => {
+    return i % numberOfWatches !== 0
+      ? CItems[CItems.length - 1].push(watch)
+      : CItems.push([watch]);
   });
 
   return (
-    <div className="mx-auto" className="container">
+    <div className="mx-auto container">
       <Carousel>
         {CItems.map((CItem, i) => (
-          <Carousel.Item>
+          <Carousel.Item key={i + CItem}>
             <div className="d-flex justify-content-around w-100">
-              {CItem.map((watchImg, j) => {
-                console.log(i + j);
-                watchUrls = [...watchUrls, watchImg.url];
-                return (
-                  <>
-                    <div
-                      className="carousel-img-anchor"
-                      onClick={() => {
-                        openLightboxOnSource(i);
-                        console.log("hey");
-                      }}
-                    >
-                      <img
-                        className="carousel-img w-100"
-                        style={{
-                          borderRadius: "15px",
-                          width: imageSize,
-                        }}
-                        src={watchImg.url}
-                        alt="First Image"
-                      />
-                      <FaSearchPlus className="searchIcon" size={60} />
-                    </div>
-                  </>
-                );
-              })}
+              {CItem.map((watchImg, j) => (
+                <div
+                  key={watchImg + j + i}
+                  className="carousel-img-anchor"
+                  onClick={() => {
+                    openLightboxOnSource(i * numberOfWatches + j);
+                    console.log(i * numberOfWatches + j);
+                  }}>
+                  <img
+                    className="carousel-img w-100"
+                    style={{
+                      borderRadius: '15px',
+                      width: imageSize,
+                    }}
+                    src={watchImg.url}
+                    alt="First "
+                  />
+                  <FaSearchPlus className="searchIcon" size={60} />
+                </div>
+              ))}
             </div>
           </Carousel.Item>
         ))}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import React, { useState, useEffect } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
-import { motion } from "framer-motion";
-import { FirstNav, SeconNav } from "./Navs";
+import { motion } from 'framer-motion';
+import { FirstNav, SeconNav } from './Navs';
 export default function Header() {
   const [showSecondNav, setShowSecondNav] = useState(true);
   useEffect(() => {
@@ -12,7 +12,7 @@ export default function Header() {
       setShowSecondNav(true);
     }
   }, []);
-  window.addEventListener("resize", function () {
+  window.addEventListener('resize', function () {
     if (window.innerWidth < 992) {
       setShowSecondNav(false);
     } else {
@@ -23,20 +23,18 @@ export default function Header() {
   return (
     <header
       style={{
-        position: "fixed",
+        position: 'fixed',
         zIndex: 9,
-        width: "100%",
-      }}
-    >
+        width: '100%',
+      }}>
       <Navbar
         style={{
-          position: "absolute",
-          width: "100%",
+          position: 'absolute',
+          width: '100%',
         }}
         bg="dark"
-        className=" po px-5 w-100vw  p-4 "
-        expand="lg"
-      >
+        className="  px-5 w-100vw  py-3 "
+        expand="lg">
         <Navbar.Brand className="text-white" href="#home">
           <motion.img
             whileTap={{ scale: 0.9 }}
@@ -47,7 +45,7 @@ export default function Header() {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="d-flex justify-content-end" style={{ width: "50vw" }}>
+          <Nav className="d-flex justify-content-end" style={{ width: '50vw' }}>
             <FirstNav />
           </Nav>
           {showSecondNav && <SeconNav />}
